0e40630315dd56833536da509e1f9fe04bfedb0e
maylogger <vtuber@maylogger.com>
2024-09-23 21:13:53 +0800

    scroll spin

--- app/header.tsx
+++ app/header.tsx
@@ -6,10 +6,11 @@ import Spline from "@splinetool/react-spline/next";
 export default function Header() {
   const { scrollY } = useScroll();
   const cocoScroll = useTransform(scrollY, [0, 1000], [0, 750]);
+  const cocoOpacity = useTransform(scrollY, [500, 1000], [1, 0]);
 
   return (
     <motion.section
-      style={{ translateY: cocoScroll }}
+      style={{ translateY: cocoScroll, opacity: cocoOpacity }}
       className="h-[600px] relative z-0"
     >
       <Spline
